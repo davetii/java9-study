0f0a59f565299507049324597218fa21db2df45b
Dave Turner <davetii@gmail.com>
2021-01-26 21:29:26 -0500

    improved imports for logging

--- module-example/src/com.greatwideweb.joke.client/com/greatwideweb/joke/client/JokeClient.java
+++ module-example/src/com.greatwideweb.joke.client/com/greatwideweb/joke/client/JokeClient.java
@@ -2,21 +2,20 @@ package com.greatwideweb.joke.client;
 
 import com.greatwideweb.joke.server.JokeServer;
 
-import java.lang.System.Logger;
 import java.util.logging.Level;
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 public class JokeClient {
 
 
-    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(JokeClient.class.getName());
+    private static final Logger log = Logger.getLogger(JokeClient.class.getName());
     public static void main(String[] args) {
-        LOGGER.info("Hello World from logger");
+        log.info("Hello World from logger");
         JokeServer server = new JokeServer();
         server.getJokes().stream().forEach( s -> {
-            LOGGER.info("setup: " + s.getSetup());
-            LOGGER.info("punchline: " + s.getPunchline());
-            LOGGER.info("\n");
+            log.info("setup: " + s.getSetup());
+            log.info("punchline: " + s.getPunchline());
+            log.info("\n");
         });
     }
 }
